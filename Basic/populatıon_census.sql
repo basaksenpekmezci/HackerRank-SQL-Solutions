cf0a0d92a1bd3d27ec8e3dc87a6b23cd98589373
Başak Şenpekmezci <117167484+basaksenpekmezci@users.noreply.github.com>
2026-01-05 13:49:46 +0300

    Create population_census.sql with query for Asia
    Add SQL query to sum populations of Asian cities

---
+++ Basic/populatıon_census.sql
@@ -0,0 +1,12 @@
+##POPULATION CENSUS
+/*
+Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+
+SELECT SUM(CITY.POPULATION)
+FROM CITY
+INNER JOIN COUNTRY 
+ON CITY.COUNTRYCODE=COUNTRY.CODE
+WHERE COUNTRY.CONTINENT="Asia"
