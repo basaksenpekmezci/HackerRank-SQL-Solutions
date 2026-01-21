0193c4bdb270030e08f02ac5fc67323a6ebc377e
Başak Şenpekmezci <117167484+basaksenpekmezci@users.noreply.github.com>
2026-01-21 23:54:50 +0300

    Add SQL query for city names filtering by vowels
    Add SQL query to select distinct city names from STATION that do not start or end with vowels.

---
+++ Basic/Weather_Observation_Station_11.sql
@@ -0,0 +1,10 @@
+##Weather Observation Station 11
+
+/*
+ Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
+ Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT CITY FROM STATION
+
+WHERE NOT CITY REGEXP '^[AEIOU].*[aeiou]$'
