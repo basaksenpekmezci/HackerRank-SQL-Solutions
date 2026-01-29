d0fcfe596443cb70aaefe712c7a6de2a81e8e599
Başak Şenpekmezci <117167484+basaksenpekmezci@users.noreply.github.com>
2026-01-29 22:32:34 +0300

    Create SQL query for total population in California
    Add SQL query to calculate total population in California cities

---
+++ Basic/Revising Aggregations_The_Sum_Function.sql
@@ -0,0 +1,9 @@
+##Revising Aggregations - The Sum Function
+
+/*
+  Query the total population of all cities in CITY where District is California.
+*/
+SELECT 
+sum(POPULATION)
+FROM CITY
+WHERE DISTRICT='California'
